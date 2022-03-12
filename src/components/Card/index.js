384fb349f2401d112aa4a48affa4d30719e33898
André Espina <contato@aespina.com>
2022-03-12 20:26:44 -0300

    First Commit

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="" />
+      </header>
+      <p>Fazer migração completa do Servidor!</p>
+    </Container>
+  );
+}
